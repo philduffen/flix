ca03155495c96c83001464bd1ef432cee6ede22c
Phil Duffen <phil@duffen.co.uk>
2014-07-14 10:11:03 +0100

    first commit - Test-First: List Movies - step 22

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = '8e2c594513a38d17457957f0f2813e227fd7e303dad32326fd6acb81ef068da8866619ffd46c0a4cb56f2fe49eaef86d3a979533713c4c3de77a97ad3c40bbc6'
