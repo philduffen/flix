03e182dbd2839f0ca73dd19bc8097de216db732a
Phil Duffen <phil@duffen.co.uk>
2014-07-19 15:36:07 +0100

    Test-First: Link Pages Exercises - step 28

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -5,16 +5,15 @@ describe  "Viewing an individual movie" do
 
 	it "shows the movie's details" do
 
-	movie = Movie.create(title: "Iron Man",
-		                      rating: "PG-13",
-		                      total_gross: 318412101.00,
-		                      description: "Tony Stark builds an armored suit to fight the throes of evil",
-		                      released_on: "2008-05-02")
+	movie = Movie.create(movie_attributes(total_gross: 300000000.00))
 
 	visit movie_url(movie)
 
-	    expect(page).to have_text(movie.title)
-			expect(page).to have_text(movie.description)
+	  expect(page).to have_text(movie.title)
+    expect(page).to have_text(movie.rating)
+    expect(page).to have_text("$300,000,000.00")
+    expect(page).to have_text(movie.description)
+    expect(page).to have_text(movie.released_on)
 	end
  
 end
