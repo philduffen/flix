fa81e5b1981793f67d61b89b487ef6ee01b4db84
Phil Duffen <phil@duffen.co.uk>
2014-07-23 17:12:04 +0100

    Forms: Creating exercises  - step 38

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,29 @@ class MoviesController < ApplicationController
 
   def update
   	@movie = Movie.find(params[:id])
-  	
-		@movie.update(params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross))
-		redirect_to movie_url
+		@movie.update(required_params)
+		redirect_to @movie 
+    # without argument this redirects to movie_url[:id]??? Hidden value in html?
+    # In edit.html.erb Form: action="/movies/1"
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(required_params)
+    @movie.save
+    redirect_to @movie
+    # without argument this  redirects to movies_url??? Hidden value in html? 
+    # In new.html.erb Form: action="/movies"
+  end
+
+  private
+
+  def required_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
   end
 
 end
