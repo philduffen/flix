5c3c91acb68335c6b49c19b93d8714a1048ffef8
Phil Duffen <phil@duffen.co.uk>
2014-08-23 18:04:06 +0100

    One-to-Many: Forms exercises - step 65

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,38 @@
 class ReviewsController < ApplicationController
 
+	before_action :set_movie
+
 	def index
-		@movie = Movie.find(params[:movie_id])
+		set_movie
 		@reviews = @movie.reviews
 	end
+
+	def new
+  	set_movie
+  	@review = @movie.reviews.new
+	end
+
+	def create
+		set_movie
+		@review = @movie.reviews.new(review_params)
+		if @review.save
+			redirect_to movie_reviews_path(@movie),
+				notice: "Thanks for your review!"
+		else
+			render :new
+		end
+	end
+
+
+	private
+
+		def review_params
+			params.require(:review).permit(:name, :comment, :stars)
+		end
+
+		def set_movie
+			@movie = Movie.find(params[:movie_id])
+		end
+
+
 end
