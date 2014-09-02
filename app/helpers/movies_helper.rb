54ca52f1af0086745e981dee7cf9ef7da359b578
Phil Duffen <phil@duffen.co.uk>
2014-09-02 22:10:25 +0100

    final  commit before deployment

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -14,4 +14,13 @@ module MoviesHelper
      image_tag(movie.image_file_name)
    end
  end
+
+ def format_average_stars(movie)
+   if movie.average_stars.nil?
+    content_tag(:strong, 'No Reviews!')
+   else
+    pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+    # "*" * movie.average_stars.round
+   end
+ end
 end
